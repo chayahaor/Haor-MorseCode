b22683f6321eeb0d6f838d5f9801f869a7ec75df
chaya <chayaz.haor@gmail.com>
2022-04-05 10:30:45 -0400

    fixed variable name errors

--- src/main/java/MorseCodeConverter.java
+++ src/main/java/MorseCodeConverter.java
@@ -10,7 +10,6 @@ public class MorseCodeConverter {
         fillMapToEnglish();
     }
 
-
     void fillMapToMorse() {
         mapToMorse.put("a", ".-");
         mapToMorse.put("b", "-...");
@@ -51,46 +50,45 @@ public class MorseCodeConverter {
     }
 
     void fillMapToEnglish() {
-        mapToMorse.put(".-", "a");
-        mapToMorse.put("-...", "b");
-        mapToMorse.put("-.-.", "c");
-        mapToMorse.put("-..", "d");
-        mapToMorse.put(".", "e");
-        mapToMorse.put("..-.", "f");
-        mapToMorse.put("--.", "g");
-        mapToMorse.put("....", "h");
-        mapToMorse.put("..", "i");
-        mapToMorse.put(".---", "j");
-        mapToMorse.put("-.-", "k");
-        mapToMorse.put(".-..", "l");
-        mapToMorse.put("--", "m");
-        mapToMorse.put("-.", "n");
-        mapToMorse.put("---", "o");
-        mapToMorse.put(".--.", "p");
-        mapToMorse.put("--.-", "q");
-        mapToMorse.put(".-.", "r");
-        mapToMorse.put("...", "s");
-        mapToMorse.put("-", "t");
-        mapToMorse.put("..-", "u");
-        mapToMorse.put("...-", "v");
-        mapToMorse.put(".--", "w");
-        mapToMorse.put("-..-", "x");
-        mapToMorse.put("-.--", "y");
-        mapToMorse.put("--..", "z");
-        mapToMorse.put("-----", "0");
-        mapToMorse.put(".----", "1");
-        mapToMorse.put("..---", "2");
-        mapToMorse.put("...--", "3");
-        mapToMorse.put("....-", "4");
-        mapToMorse.put(".....", "5");
-        mapToMorse.put("-....", "6");
-        mapToMorse.put("--...", "7");
-        mapToMorse.put("---..", "8");
-        mapToMorse.put("----.", "9");
+        mapToEnglish.put(".-", "a");
+        mapToEnglish.put("-...", "b");
+        mapToEnglish.put("-.-.", "c");
+        mapToEnglish.put("-..", "d");
+        mapToEnglish.put(".", "e");
+        mapToEnglish.put("..-.", "f");
+        mapToEnglish.put("--.", "g");
+        mapToEnglish.put("....", "h");
+        mapToEnglish.put("..", "i");
+        mapToEnglish.put(".---", "j");
+        mapToEnglish.put("-.-", "k");
+        mapToEnglish.put(".-..", "l");
+        mapToEnglish.put("--", "m");
+        mapToEnglish.put("-.", "n");
+        mapToEnglish.put("---", "o");
+        mapToEnglish.put(".--.", "p");
+        mapToEnglish.put("--.-", "q");
+        mapToEnglish.put(".-.", "r");
+        mapToEnglish.put("...", "s");
+        mapToEnglish.put("-", "t");
+        mapToEnglish.put("..-", "u");
+        mapToEnglish.put("...-", "v");
+        mapToEnglish.put(".--", "w");
+        mapToEnglish.put("-..-", "x");
+        mapToEnglish.put("-.--", "y");
+        mapToEnglish.put("--..", "z");
+        mapToEnglish.put("-----", "0");
+        mapToEnglish.put(".----", "1");
+        mapToEnglish.put("..---", "2");
+        mapToEnglish.put("...--", "3");
+        mapToEnglish.put("....-", "4");
+        mapToEnglish.put(".....", "5");
+        mapToEnglish.put("-....", "6");
+        mapToEnglish.put("--...", "7");
+        mapToEnglish.put("---..", "8");
+        mapToEnglish.put("----.", "9");
     }
 
     public String toMorse(String english) {
-        MorseCodeConverter converter = new MorseCodeConverter();
         english = english.toLowerCase();
         StringBuilder sb = new StringBuilder();
         String[] inpChar = english.split("");
